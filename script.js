4f2e6ec4e99ebc1d762ab56244649000fb3e9811
denyskh4991 <denyskh4991@gmail.com>
2024-08-21 21:45:05 +0200

    Formating the code

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const inputBox  = document.getElementById("input-box")
-const listContainer  = document.getElementById("list-container")
+const inputBox = document.getElementById("input-box")
+const listContainer = document.getElementById("list-container")
 
 function addTask() {
     if (inputBox.value === '') {
         alert("You must write something!");
-    }
-    else {
+    } else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
@@ -21,8 +20,7 @@ listContainer.addEventListener("click", function (e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
         saveData()
-    }
-    else if (e.target.tagName === "SPAN") {
+    } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
         saveData();
     }
@@ -35,4 +33,5 @@ function saveData() {
 function showTask() {
     listContainer.innerHTML = localStorage.getItem("data");
 }
+
 showTask();
